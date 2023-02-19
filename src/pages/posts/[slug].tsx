b3554ab0f7b9adfc68b06a64a11f4f336af3d7be
kalkanozgur <ahmetozgurkalkan@gmail.com>
2023-02-19 14:22:07 +0300

    deleted:    src/lib/renderer/a.js 	modified:   src/pages/posts/[slug].tsx

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -24,7 +24,6 @@ export default function BlogPage({ post }: any) {
                 <p className='text-white'>{post.metadata.tags.join(', ')}</p>
                 <h1>post</h1>
                 {notionRenderer({ blocks: post.blocks.results })}
-                {/* {JSON.stringify(post)} */}
               </>
             )}
           </div>
